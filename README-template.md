18b191f3cc1c2e60a1d375281dd1813c4b5d7b00
Beto Villalba <betovill26@gmail.com>
2024-10-09 14:50:43 -0300

    finish proyect

--- README-template.md
+++ README-template.md
@@ -1,6 +1,7 @@
 # Frontend Mentor - Blogr landing page solution
 
-This is a solution to the [Blogr landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/blogr-landing-page-EX2RLAApP). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+This is a solution to the [Blogr landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/blogr-landing-page-EX2RLAApP). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
 
 ## Table of contents
 
@@ -16,7 +17,6 @@ This is a solution to the [Blogr landing page challenge on Frontend Mentor](http
 - [Author](#author)
 - [Acknowledgments](#acknowledgments)
 
-**Note: Delete this note and update the table of contents based on what sections you keep.**
 
 ## Overview
 
@@ -24,20 +24,13 @@ This is a solution to the [Blogr landing page challenge on Frontend Mentor](http
 
 Users should be able to:
 
-- View the optimal layout for the site depending on their device's screen size
+- View the optimal layout for the site depending on their device's screen size 
 - See hover states for all interactive elements on the page
 
 ### Screenshot
 
-![](./screenshot.jpg)
+![Cropped Scream shot](./capturas/captura-cortado.png)
 
-Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
-
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
-
-Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
-
-**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
 
 ### Links
 
--- js/main.js
+++ js/main.js
@@ -3,42 +3,96 @@ const navbar = document.querySelector('#navbar');
 const btnToggleMenu = navbar.querySelector('#openMenu');
 const navbarContent = navbar.querySelector('#navbarContent');
 const btnDisplaySubmenu = navbar.querySelectorAll('.navbar__toggle__button');
+const desktopSubmenus = navbar.querySelectorAll('.navbar__content__list__inner');
 
-const hamburguerIcon = './images/icon-hamburger.svg';
-const closeIcon = './images/icon-close.svg';
-const arrowUp = './images/icon-arrow-dark.svg';
-const arrowDown = './images/icon-arrow-light.svg';
 
+const icon = {
+    hamburguer: './images/icon-hamburger.svg',
+    close: './images/icon-close.svg',
+    arrowUp: './images/icon-arrow-dark.svg',
+    arrowDown: './images/icon-arrow-light.svg'
+}
+
+
+/* toggle mobile menu */
 btnToggleMenu.addEventListener('click', (e) => {
     const img = btnToggleMenu.firstElementChild;
 
-    if (navbarContent.classList.contains('hidden')) {
+    const isHidden = navbarContent.classList.contains('hidden');
+
+    if (isHidden) {
         navbarContent.classList.remove('hidden');
         navbarContent.classList.add('visible');
-        img.src = closeIcon;
-    } else if (navbarContent.classList.contains('visible')) {
+        img.src = icon.close;
+    } else {
         navbarContent.classList.remove('visible');
         navbarContent.classList.add('hidden');
-        img.src = hamburguerIcon;
+        img.src = icon.hamburguer;
     }
 
 });
 
+/* toggle submenu mobile and desktop */
+const toggleSubmenu = (btn, submenu) => {
+
+    const isMobile = window.matchMedia('(max-width: 1024px)').matches;
+    const isHidden = submenu.classList.contains('max-h-0') || submenu.classList.contains('hidden');
+    const img = btn.firstElementChild;
+    if (isHidden && isMobile) {
+        img.src = icon.arrowDown;
+        submenu.classList.remove('max-h-0');
+        submenu.classList.add('max-h-full');
+    } else if (!isHidden && isMobile) {
+        img.src = icon.arrowUp;
+        submenu.classList.remove('max-h-full');
+        submenu.classList.add('max-h-0');
+    } else if (isHidden && !isMobile) {
+        img.src = icon.arrowDown;
+        submenu.classList.remove('hidden');
+        submenu.classList.add('visible');
+    } else if (!isHidden && !isMobile) {
+        img.src = icon.arrowUp;
+        submenu.classList.remove('visible');
+        submenu.classList.add('hidden');
+    }
+
+}
+
 btnDisplaySubmenu.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        const img = btn.firstElementChild;
-
+    btn.addEventListener('click', () => {
         const submenu = btn.nextElementSibling;
-        if (submenu.classList.contains('max-h-0')) {
-            img.src = arrowDown;
-            submenu.classList.remove('max-h-0');
-            submenu.classList.add('max-h-full');
-
-
-        } else if (submenu.classList.contains('max-h-full')) {
-            img.src = arrowUp;
-            submenu.classList.remove('max-h-full');
-            submenu.classList.add('max-h-0');
-        }
+        toggleSubmenu(btn, submenu);
     });
 });
+
+
+/* handle media queries */
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarContent = document.querySelector('#navbarContent');
+    const navbarSubmenu = navbarContent.querySelectorAll('.navbar__content__list__inner')
+    const mql = window.matchMedia("(min-width: 1024px)");
+
+
+    const handleNavbarVisibility = (mql) => {
+        if (mql.matches) {
+            navbarContent.classList.remove('hidden');
+
+            navbarSubmenu.forEach((submenu) => {
+                submenu.classList.remove('max-h-0');
+                submenu.classList.add('hidden');
+            });
+        } else {
+            navbarContent.classList.add('hidden');
+
+            navbarSubmenu.forEach((submenu) => {
+                submenu.classList.add('max-h-0');
+                submenu.classList.remove('hidden');
+            });
+        }
+    };
+
+    handleNavbarVisibility(mql);
+
+    mql.addEventListener('change', (e) => handleNavbarVisibility(e));
+});
+
